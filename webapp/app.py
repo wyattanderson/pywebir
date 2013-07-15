5e1f9e1e317d81a643a341cab6aa53afb6113f07
Wyatt Anderson <wyatt@voxy.com>
2013-07-14 20:10:31 -0400

    don't do this

--- webapp/app.py
+++ webapp/app.py
@@ -61,23 +61,23 @@ def do_button(button):
         'state.json'), 'w') as state_save:
         json.dump(state.export(), state_save)
 
-    # if should_send:
-    #     button_data = buttons[button]
-    #     buf = bytearray(base64.b64decode(button_data['irdata']))
-    #     sp = serial.Serial('/dev/ttyACM0')
-    #     sp.write("\0\0\0\0\0")
-    #     sleep()
-    #     sp.write("S")
-    #     sleep()
-    #     sp.write("\x03")
-    #     sleep()
-    #     sp.write(buf)
-    #     sleep()
-    #     sp.write("\0")
-    #     sleep()
-    #     sp.write("S")
-    #     sleep()
-    #     sp.close()
+    if should_send:
+        button_data = buttons[button]
+        buf = bytearray(base64.b64decode(button_data['irdata']))
+        sp = serial.Serial('/dev/ttyACM0')
+        sp.write("\0\0\0\0\0")
+        sleep()
+        sp.write("S")
+        sleep()
+        sp.write("\x03")
+        sleep()
+        sp.write(buf)
+        sleep()
+        sp.write("\0")
+        sleep()
+        sp.write("S")
+        sleep()
+        sp.close()
 
     return jsonify(state.export())
 
